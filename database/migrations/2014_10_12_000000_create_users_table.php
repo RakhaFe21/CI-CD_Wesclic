ebc86627f9e02b8781fda8209940b5c2edbb5a05
wizurai1302 <midhamk1302@gmail.com>
2023-08-02 21:46:20 +0700

    Fix(migration) users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('slug')->nullable();
             $table->string('email')->unique();
+            $table->bigInteger('nik',16);
             $table->enum('user_type',['Student','Instructor','Admin'])->nullable(); //this is identify user type
             //1=Admin,2=Instructor,3=Student
             $table->timestamp('email_verified_at')->nullable();
@@ -26,13 +27,14 @@ class CreateUsersTable extends Migration
             $table->boolean('banned')->default(false); // User cannot login if banned
             $table->string('provider_id')->nullable(); // this is for login fb,g+ etc
             $table->string('image')->nullable()->default('uploads/user/user.png');
-            
+            $table->string('remember_token',100)->default();
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
             $table->longText('zoom_email')->nullable(); // for Zoom
             $table->longText('jwt_token')->nullable(); // for Zoom
-            
-            $table->rememberToken();
-            $table->timestamps();
+            $table->rememberToken();     
         });
+        
     }
 
     /**

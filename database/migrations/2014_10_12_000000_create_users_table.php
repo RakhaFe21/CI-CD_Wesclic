6ccbb5de3867b39acadf6001442139ece620156e
wizurai1302 <midhamk1302@gmail.com>
2023-08-03 22:30:53 +0700

    fic(migration) users dan courses

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,23 +18,19 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('slug')->nullable();
             $table->string('email')->unique();
-            $table->bigInteger('nik',16);
-            $table->enum('user_type',['Student','Instructor','Admin'])->nullable(); //this is identify user type
-            //1=Admin,2=Instructor,3=Student
+            $table->bigInteger('nik')->unique();
+            $table->enum('user_type', ['Student', 'Instructor', 'Admin'])->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('verified')->default(false);
             $table->string('password')->nullable();
-            $table->boolean('banned')->default(false); // User cannot login if banned
-            $table->string('provider_id')->nullable(); // this is for login fb,g+ etc
+            $table->boolean('banned')->default(false);
+            $table->string('provider_id')->nullable();
             $table->string('image')->nullable()->default('uploads/user/user.png');
-            $table->string('remember_token',100)->default();
-            $table->timestamp('created_at');
-            $table->timestamp('updated_at');
-            $table->longText('zoom_email')->nullable(); // for Zoom
-            $table->longText('jwt_token')->nullable(); // for Zoom
-            $table->rememberToken();     
+            $table->longText('zoom_email')->nullable();
+            $table->longText('jwt_token')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
         });
-        
     }
 
     /**

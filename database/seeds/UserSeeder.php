f31c6f8e78c7e15539473a9942a9c8e5832ca226
Rassid <rassiddical@gmail.com>
2023-08-08 15:17:44 +0700

    Fix(Profil Admin) Created Updated

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,11 +17,14 @@ class UserSeeder extends Seeder
       DB::table('users')->insert([
           'name' => 'Admin',
           'email' => 'admin@mail.com',
+          'nik' => '325634634',
           'password' => Hash::make('12345678'),
           'user_type' => 'Admin',
           'banned' => false,
           'verified' => true,
           'image' => 'uploads/user/nfkUiXvcdhYfWol7esVLtUxZ0kOqTkvC2FMsYiNa.png',
+          'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+          'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
       ]);
     }
 }

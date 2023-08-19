c0fe2f2e1076efade503cb1c99b3679a06429b72
wizurai1302 <midhamk1302@gmail.com>
2023-08-19 10:40:22 +0700

    Fix(instructor)

--- app/Http/Controllers/Instructor/InstructorController.php
+++ app/Http/Controllers/Instructor/InstructorController.php
@@ -8,13 +8,13 @@ use App\Model\Package;
 use App\Model\Instructor;
 use App\Model\VerifyUser;
 use App\NotificationUser;
+use phpseclib\Crypt\Hash;
 use App\Model\AdminEarning;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use App\Model\PackagePurchaseHistory;
 use Illuminate\Support\Facades\Schema;
@@ -183,70 +183,86 @@ class InstructorController extends Controller
 
     public function instructor_store(Request $request)
     {
-        // Periksa apakah mode demo aktif
         if (env('DEMO') === "YES") {
-            Alert::warning('warning', 'This is for demo purposes only');
+            Alert::warning('warning', 'This is demo purpose only');
             return back();
         }
-    
-        // Validasi input
+
         $request->validate([
+            //'package_id' => 'required',
             'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required|min:8',
-            'confirm_password' => 'required|same:password',
-            'nik' => 'required|unique:instructors', // Pastikan NIK unik di tabel instructors
+            'email' => ['required', 'unique:users'],
+            'password' => ['required', 'min:8'],
+            'confirm_password' => 'required|required_with:password|same:password',
         ], [
+            //'package_id.required' => translate('Please select a package'),
             'name.required' => translate('Name is required'),
             'email.required' => translate('Email is required'),
-            'email.email' => translate('Invalid email format'),
-            'email.unique' => translate('Email already exists.'),
+            'email.unique' => translate('Email is already exist.'),
             'password.required' => translate('Password is required'),
-            'password.min' => translate('Password must be at least 8 characters'),
+            'password.min' => translate('Password must be minimum 8 characters'),
             'confirm_password.required' => translate('Please confirm your password'),
-            'confirm_password.same' => translate('Passwords do not match'),
-            'nik.required' => translate('NIK is required'),
-            'nik.unique' => translate('NIK already exists.'),
+            'confirm_password.same' => translate('Password did not match'),
         ]);
-    
-        try {
-            // Create a new user
-            $user = new User();
-            $user->name = $request->name;
-            $user->email = $request->email;
-            $user->nik = $request->nik;
-            $user->password = Hash::make($request->password);
-            $user->user_type = 'Instructor';
-            $user->save();
+        /*get package value*/
+        $package = Package::where('id', 1)->firstOrFail();
+        // $package = Package::where('id', $request->package_id)->firstOrFail();
+        //create user for login
 
-            // Create a new instructor
-            $instructor = new Instructor();
-            $instructor->name = $request->name;
-            $instructor->email = $request->email;
-            $instructor->nik = $request->nik;
-            $instructor->user_id = $user->id;
-            $instructor->save();
-
-            // Create a purchase history (you might need to adjust this part)
-            $package = Package::find(1); // You need to replace 1 with the correct package ID
-            $purchase = new PackagePurchaseHistory();
-            $purchase->amount = $package->price; // Adjust this based on your logic
-            $purchase->payment_method = $request->payment_method; // Adjust this based on your logic
-            $purchase->package_id = $package->id; // Adjust this based on your logic
-            $purchase->user_id = $user->id;
-            $purchase->save();
-    
-            // Hitung pendapatan admin (Anda perlu mengimplementasikan ini)
-    
-            // Kirim email verifikasi
-            $user->notify(new VerifyNotifications($user));
-    
-            Session::flash('message', translate("Registration done successfully."));
-            return back();
-        } catch (\Exception $exception) {
-            // Tangani pengecualian jika diperlukan
-            // Misalnya, tampilkan pesan kesalahan atau log pesan kesalahan
-            return back()->withErrors(['error' => 'Registration failed. Please try again.']);
+        $slug_name = Str::slug($request->name);
+        /*check the sulg */
+        $users = User::where('slug', $slug_name)->get();
+        if ($users->count() > 0) {
+            $slug_name = $slug_name . ($users->count() + 1);
         }
+        $user = new User();
+        $user->slug = $slug_name;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = \Illuminate\Support\Facades\Hash::make($request->password);
+        $user->user_type = 'Instructor';
+        $user->save();
+
+        //save data in instructor
+        $instructor = new Instructor();
+        $instructor->name = $request->name;
+        $instructor->email = $request->email;
+        $instructor->package_id = 1;
+        $instructor->user_id = $user->id;
+        $instructor->save();
+
+
+        //add purchase history
+        $purchase = new PackagePurchaseHistory();
+        $purchase->amount = $package->price;
+        $purchase->payment_method = $request->payment_method;
+        $purchase->package_id = 1;
+        $purchase->user_id = $user->id;
+        $purchase->save();
+
+
+        //todo::admin Earning calculation
+        $admin = new AdminEarning();
+        $admin->amount = $package->price;
+        $admin->purposes = "Sale Package";
+        $admin->save();
+
+        try {
+
+            $user->notify(new InstructorRegister());
+
+            VerifyUser::create([
+                'user_id' => $user->id,
+                'token' => sha1(time())
+            ]);
+            //send verify mail
+            $user->notify(new VerifyNotifications($user));
+        } catch (\Exception $exception) {
+        }
+
+
+        Session::flash('message', translate("Registration done successfully."));
+        return back();
     }
-}    
+    //END
+}
